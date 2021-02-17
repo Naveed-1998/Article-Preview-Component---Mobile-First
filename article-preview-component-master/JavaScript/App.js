eea9729db249c45da5ab13cee43c48071bd015db
Naveed-1998 <72744876+Naveed-1998@users.noreply.github.com>
2021-02-17 12:38:28 +0000

    Add files via upload

---
+++ article-preview-component-master/JavaScript/App.js
@@ -0,0 +1,6 @@
+function myFunction() {
+    var popup = document.getElementById("myPopup");
+    popup.classList.toggle("show");
+    var share = document.getElementById("toggle");
+    share.classList.toggle("show-2")
+}
